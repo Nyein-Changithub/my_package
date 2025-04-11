ac55e6d2e8978fd37bcbe9a14de5c21f232c7de0
Nyein Chan Aung <nyeinchanos400@gmail.com>
2025-04-11 10:44:56 +0630

    vendor name change and code updated

--- src/routes.php
+++ src/routes.php
@@ -6,15 +6,6 @@ use Pkg\MyPackage\MyPackageController;
 Route::get('my-package', function () {
     return 'Hello from the myPackage package';
 });
-
-// Route::get('add/{a}/{b}',[ Pkg\MyPackage\MyPackageController::class,'add']);
-// Route::get('subtract/{a}/{b}',[ Pkg\MyPackage\MyPackageController::class ,'subtract']);
-
-Route::prefix('mypackage')->group(function () {
-    Route::get('/', [MyPackageController::class, 'index']);
-    Route::post('/upload', [MyPackageController::class, 'upload'])->name('myPackage.upload');
-});
-
 Route::post('/mypackage/cloudflare-upload', [MyPackageController::class, 'uploadToCloudflare'])->name('myPackage.cloudflare.upload');
 Route::get('/mypackage/cloudflare-image/{id}', [MyPackageController::class, 'getImageFromCloudflare'])->name('myPackage.cloudflare.image');
 Route::delete('/mypackage/cloudflare-delete/{id}', [MyPackageController::class, 'deleteFromCloudflare'])->name('myPackage.cloudflare.delete');
